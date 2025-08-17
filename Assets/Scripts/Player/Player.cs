dc8d1624a9084808b2c81b508096c21ec41e80d1
deepa-mylavarapu <deepa.adidamu@gmail.com>
2025-08-18 01:21:56 +0200

    JUST WORKED AS IN RECORDING
    WORKED OUT

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,24 @@ public class Player : NetworkBehaviour
 {
     public float moveSpeed = 5f;
 
+    public bool isGrounded = true;
+
+    public float jumpForce = 2.0f;
+
+    public float groundDistanceCheck = 0.1f;
+
+    public LayerMask groundMask;
+
+    private void Awake()
+    {
+        groundMask = LayerMask.GetMask("Ground");
+    }
+
+
+    [SerializeField]
+    private Rigidbody rigidBody;
+
+
     private void Update()
     {
         // Only process input for the local player
@@ -16,17 +34,50 @@ public class Player : NetworkBehaviour
             Input.GetAxis("Vertical")
         );
 
+        // if the space key is pressed the player is grounded
+        if(Input.GetKeyDown(KeyCode.Space)&& IsGrounded())
+        {
+            Debug.Log("CLIENT:I want to jump");
+            Jump();
+            JumpServerRpc();   
+        }
+
 
         Vector3 move = input * moveSpeed * Time.deltaTime;
 
         // Send the movement to the server
-        MoveServerRpc(move);
+         MoveServerRpc(move);
     }
-
+    // server function that calls the jump function to perform logic
     [ServerRpc]
     private void MoveServerRpc(Vector3 move, ServerRpcParams rpcParams = default)
     {
         // Apply movement on the server
         transform.position += move;
     }
+    [ServerRpc]
+    private void JumpServerRpc()
+    {
+        Debug.Log("SERVER:The player wants to jump");
+        // Actually jump
+        Jump();
+    }
+    // apply velocity to the rigidbody to jump (works on either server or client)
+    private void Jump()
+    {
+        var vel = rigidBody.linearVelocity;
+        vel.y = 0f;
+        vel.y += jumpForce;
+        rigidBody.linearVelocity = vel;
+    }
+    bool IsGrounded()
+    {
+        Vector3 origin = transform.position + Vector3.up * 0.05f;
+        float radius = 0.25f;
+        RaycastHit hit;
+        // perform spherecast from the origin and the radius provided only return result
+        // that it hits the ground layermask
+        return Physics.SphereCast(origin, radius, Vector3.down, out hit, groundDistanceCheck, groundMask, QueryTriggerInteraction.Ignore);
+    }
+
 }
